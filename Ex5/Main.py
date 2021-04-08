3c0b5c424dd4e7fb31ba54ce2d2af3ccbf82650a
Player Ghost <sousaitalo12@gmail.com>
2021-04-08 11:09:09 -0300

    Add files via upload

---
+++ Ex5/Main.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from GrafosMatriz import GrafosMatriz
+
+if __name__ == '__main__':
+
+    grafo = GrafosMatriz()
+    grafo.BFS(1)
+
+
+
+
